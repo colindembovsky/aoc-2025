021ac58abc2642e715d57b7a53825a09d07d74d5
Colin Dembovsky <colindembovsky@github.com>
2025-12-05 21:46:04 -0600

    feat: Update input handling and logic for day 03 to calculate total jolts

--- day03/program.ts
+++ day03/program.ts
@@ -2,10 +2,23 @@ import { loadInput, dayName, Difficulty } from "../utils/readUtils";
 import { timedExecute } from "../utils/miscUtils";
 
 let day = dayName(__dirname);
-let contents = loadInput(__dirname, Difficulty.EASY);
+let contents = loadInput(__dirname, Difficulty.HARD);
 let lines = contents.split("\n");
 
 console.log(`==== ${day}: PART 1 ====`);
 timedExecute(() => {
-    
+    let totalJolts = 0;
+    for(let bank of lines) {
+        let digits = bank.split("").map(Number);
+        let max = Math.max(...digits);
+        let maxIndex = bank.split("").indexOf(max.toString());
+        if (maxIndex === digits.length - 1) {
+            max = Math.max(...digits.slice(0, maxIndex));
+            maxIndex = bank.split("").indexOf(max.toString());
+        }
+        let secondMax = Math.max(...digits.slice(maxIndex + 1));
+        let jolts = parseInt(`${max}${secondMax}`);
+        totalJolts += jolts;
+    }
+    console.log(`Total jolts: ${totalJolts}`);
 });
