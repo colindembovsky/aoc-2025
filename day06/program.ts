5e4d24b261ebf29ccdec424b78cc4de886cbee2c
Colin Dembovsky <colindembovsky@github.com>
2025-12-06 12:19:40 -0600

    feat: Refactor program logic for part 1 and part 2 to improve clarity and structure

--- day06/program.ts
+++ day06/program.ts
@@ -4,25 +4,23 @@ import { timedExecute } from "../utils/miscUtils";
 let day = dayName(__dirname);
 let contents = loadInput(__dirname, Difficulty.HARD);
 let lines = contents.split("\n");
-// split each line by 1 or more spaces
-let splitLines = lines.map(line => line.split(/\s+/));
-// remove empty strings
-splitLines = splitLines.map(line => line.filter(item => item !== ""));
-
-// create an array of columns (1st item in each line is column 1, 2nd item is column 2, etc.)
-let columns = [];
-for (let i = 0; i < splitLines[0].length; i++) {
-    let column = [];
-    for (let j = 0; j < splitLines.length; j++) {
-        if (splitLines[j][i] !== "") {
-            column.push(splitLines[j][i]);
-        }
-    }
-    columns.push(column);
-}
 
 console.log(`==== ${day}: PART 1 ====`);
 timedExecute(() => {
+    let splitLines = lines.map(line => line.split(/\s+/));
+    splitLines = splitLines.map(line => line.filter(item => item !== ""));
+
+    let columns = [];
+    for (let i = 0; i < splitLines[0].length; i++) {
+        let column = [];
+        for (let j = 0; j < splitLines.length; j++) {
+            if (splitLines[j][i] !== "") {
+                column.push(splitLines[j][i]);
+            }
+        }
+        columns.push(column);
+    }
+
     let total = 0;
     columns.forEach(c => {
         // get and remove the last item in the column
@@ -31,13 +29,36 @@ timedExecute(() => {
             // perform addition on the remaining items in the column
             let sum = c.reduce((acc, val) => acc + Number(val), 0);
             total += sum;
-            console.log(sum);
+            //console.log(sum);
         } else if (op === "*") {
             // perform multiplication on the remaining items in the column
             let product = c.reduce((acc, val) => acc * Number(val), 1);
             total += product;
-            console.log(product);
+            //console.log(product);
         }
     });
+    console.log(`Total: ${total}`);
+});
+
+console.log(`==== ${day}: PART 2 ====`);
+timedExecute(() => {
+    let curIndex = lines[0].length - 1;
+    let total = 0;
+    let vals: number[] = [];
+    while (curIndex >= 0) {
+        let num = "";
+        for (let i = 0; i < lines.length - 1; i++) {
+            num += lines[i][curIndex];
+        }
+        vals.push(Number(num));
+        
+        let op = lines[lines.length - 1][curIndex].trim();
+        if (op === "+" || op === "*") {
+            total += op === "*" ? vals.reduce((acc, val) => acc * val, 1) : vals.reduce((acc, val) => acc + val, 0);
+            curIndex--;
+            vals = [];
+        }
+        curIndex--;
+    }
     console.log(`Total: ${total}`);
 });
