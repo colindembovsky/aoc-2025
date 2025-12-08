6c70016f7efcaf2dfae493a08273f5881613e64e
Colin Dembovsky <colindembovsky@github.com>
2025-12-08 14:31:30 -0600

    feat: Update input difficulty to EASY and restructure pathfinding logic for clarity

--- day07/program.ts
+++ day07/program.ts
@@ -3,14 +3,14 @@ import { timedExecute } from "../utils/miscUtils";
 import { CharMap, Direction } from "../utils/mapUtils";
 
 let day = dayName(__dirname);
-let contents = loadInput(__dirname, Difficulty.HARD);
+let contents = loadInput(__dirname, Difficulty.EASY);
 let lines = contents.split("\n");
 let map = new CharMap(lines);
+let start = map.getAllPositionsWithValue("S")[0];
+let beams = [start.move(Direction.down)];
 
 console.log(`==== ${day}: PART 1 ====`);
 timedExecute(() => {
-    let start = map.getAllPositionsWithValue("S")[0];
-    let beams = [start.move(Direction.down)];
     let seen = new Set<string>();
     seen.add(beams[0].toString());
     let splits = 0;
@@ -40,4 +40,41 @@ timedExecute(() => {
         }
     }
     console.log(`Number of splits: ${splits}`);
+});
+
+console.log(`==== ${day}: PART 2 ====`);
+timedExecute(() => {
+    let cache = new Map<string, number>();
+    function countPaths(pos: typeof start): number {
+        let key = pos.toString();
+        if (cache.has(key)) {
+            return cache.get(key)!;
+        }
+        
+        let nextDown = pos.down();
+        if (nextDown.row >= map.height) {
+            cache.set(key, 1);
+            return 1;
+        }
+        
+        let nextValue = map.get(nextDown);
+        let paths = 0;
+        if (nextValue === ".") {
+            paths = countPaths(nextDown);
+        } else if (nextValue === "^") {
+            let leftPos = nextDown.left();
+            if (leftPos.col >= 0) {
+                paths += countPaths(leftPos);
+            }
+            let rightPos = nextDown.right();
+            if (rightPos.col < map.width) {
+                paths += countPaths(rightPos);
+            }
+        }
+        cache.set(key, paths);
+        return paths;
+    }
+    
+    let totalPaths = countPaths(start.move(Direction.down));
+    console.log(`Number of paths: ${totalPaths}`);
 });
