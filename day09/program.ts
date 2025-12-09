2fe25be98929d7c25dcac4aa4d09f4aed13cd3e2
Colin Dembovsky <colindembovsky@github.com>
2025-12-09 16:40:26 -0600

    solve 9.1

--- day09/program.ts
+++ day09/program.ts
@@ -2,10 +2,35 @@ import { loadInput, dayName, Difficulty } from "../utils/readUtils";
 import { timedExecute } from "../utils/miscUtils";
 
 let day = dayName(__dirname);
-let contents = loadInput(__dirname, Difficulty.EASY);
+let contents = loadInput(__dirname, Difficulty.HARD);
 let lines = contents.split("\n");
+
+let points: { x: number, y: number }[] = [];
+for (let line of lines) {
+    let [x, y] = line.split(",").map(Number);
+    points.push({ x, y });
+}
+
+function area(pointA: { x: number, y: number }, pointB: { x: number, y: number }) {
+    return (Math.abs(pointA.x - pointB.x) + 1) * (Math.abs(pointA.y - pointB.y) + 1);
+}
 
 console.log(`==== ${day}: PART 1 ====`);
 timedExecute(() => {
-    
+    let maxArea = 0;
+    for (let i = 0; i < points.length; i++) {
+        for (let j = i + 1; j < points.length; j++) {
+            const currentArea = area(points[i], points[j]);
+            if (currentArea > maxArea) {
+                maxArea = currentArea;
+            }
+        }
+    }
+
+    console.log(`The maximum area is ${maxArea}`);
+});
+
+console.log(`==== ${day}: PART 2 ====`);
+timedExecute(() => {
+
 });
